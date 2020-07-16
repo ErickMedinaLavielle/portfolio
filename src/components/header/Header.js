626eebaa06cb4feda26de6e9ed9513b6fa2a761e
ErickMedinaLavielle <erickmedlav@gmail.com>
2020-07-15 21:34:58 -0500

    Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <a href="#" className="logo">
+        Erick Lavielle
+      </a>
+      <div className="menu"></div>
+    </div>
+  );
+}
